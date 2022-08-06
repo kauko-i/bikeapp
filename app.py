ded8a080625ccf55db101ce34e7e2ef54bb7c5de
Ilari Kauko <ilarikauko@gmail.com>
2022-08-06 16:49:20 +0300

    add journeys page and fetch data to it from database, handle id conflict in uploading stations

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ ALLOWED_EXTENSIONS = ['csv']
 JOURNEY_HEADER = 'Departure,Return,Departure station id,Departure station name,Return station id,Return station name,Covered distance (m),Duration (sec.)\n'
 STATION_HEADER = 'FID,ID,Nimi,Namn,Name,Osoite,Adress,Kaupunki,Stad,Operaattor,Kapasiteet,x,y\n'
 DATABASE_URL = os.environ['DATABASE_URL']
+JOURNEY_LIMIT = 100
+METERS_IN_KILOMETER = 1000
+SECONDS_IN_MINUTE = 60
 
 def allowed_filename(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -84,12 +87,31 @@ def upload():
                     except ValueError:
                         continue
                     cur.execute('''INSERT INTO stations(id,nimi,namn,name,osoite,adress,kaupunki,stad,operaattori,kapasiteetti,lat,lon)
-                    VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)''',(id,nimi,namn,name,osoite,adress,kaupunki,stad,operator,capacity,lat,lon),)
+                    VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) ON CONFLICT DO NOTHING''',
+                    (id,nimi,namn,name,osoite,adress,kaupunki,stad,operator,capacity,lat,lon),)
             os.remove(secure_name)
         con.commit()
         cur.close()
         con.close()
-    return Response(render_template('upload.html'))
+    return render_template('upload.html')
+
+@app.route('/journeys')
+def journeys():
+    con = psycopg2.connect(DATABASE_URL, sslmode='require')
+    cur = con.cursor()
+    cur.execute('''
+    SELECT departures.name, returns.name, distance, duration
+    FROM journeys
+    JOIN stations departures ON journeys.departure_station = departures.id
+    JOIN stations returns ON journeys.return_station = returns.id
+    LIMIT %s
+    ''',(JOURNEY_LIMIT,))
+    rows = cur.fetchall()
+    cur.close()
+    con.close()
+    row_list = [{'departure_station':str(row[0]),'return_station':str(row[1]),'distance':str(float(row[2])/METERS_IN_KILOMETER),
+    'duration':str(float(row[3])/SECONDS_IN_MINUTE)} for row in rows]
+    return render_template('journeys.html', journeys=row_list)
 
 @app.route('/')
 def index():
